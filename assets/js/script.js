8f58baf67b9bfe5a8a19e34b2446c0eac17b5a38
Moonyll <s.pouwel@laposte.net>
2019-01-11 15:51:49 +0100

    Jsss1

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+﻿function bordure()
+{
+document.getElementById('image1').style.border = "3px solid red";
+//img.style.border = "none";
+}
+function sbordure() {
+document.getElementById('image1').style.border = "none";
+ }
